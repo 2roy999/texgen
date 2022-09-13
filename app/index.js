fd24c3b540c80401fd6a3d7559c7ea883b3589a4
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2022-09-14 00:31:51 +0300

    Adding templates

--- app/index.js
+++ app/index.js
@@ -1,15 +1,73 @@
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
-  async type () {
-    const answers = await this.prompt([
+  async run () {
+    const { author, email, address } = await this.prompt([
       {
         type: 'list',
         name: 'type',
-        choices: ['article', 'notes', 'beamer']
+        choices: ['article'],
+        message: 'Choose a type of project:'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Type author\'s full name:',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Type author\'s email:',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'address',
+        message: 'Type author\'s institute address:',
+        store: true
       }
     ])
-    
-    
+
+    const { hasAppendix } = await this.prompt([
+      {
+        type: 'confirm',
+        name: 'hasAppendix',
+        message: 'Does the article has appendix?',
+        default: false
+      }
+    ])
+
+    const templates = [
+      { src: 'github/workflows/main.yml', dest: '.github/workflows/main.yml' },
+      'abstract.tex',
+      'main.tex',
+      'acknowledgments.tex',
+      { src: 'gitignore', dest: '.gitignore' },
+      'introduction.tex',
+      'main.bib',
+      'main.tex',
+      'preliminaries.tex',
+      'root.tex',
+      'shortcuts.tex'
+    ]
+
+    if (hasAppendix) {
+      templates.push('appendix.tex')
+    }
+
+    const props = { author, email, address, hasAppendix }
+
+    templates.map(template => {
+      const { src, dest } = (typeof template === 'string')
+        ? { src: template, dest: template }
+        : template
+
+      return this.fs.copyTpl(
+        this.templatePath(src),
+        this.destinationPath(dest),
+        props
+      )
+    })
   }
 }
